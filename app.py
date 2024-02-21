779ccc31a3ee8b9c042cb6cd25149ba8645f8151
Lorenzo Fernandez <99827632+lorenzofdz@users.noreply.github.com>
2024-02-21 02:21:26 -0600

    Reemplazar los botones por casillas de verificacion

--- app.py
+++ app.py
@@ -5,23 +5,23 @@ import streamlit as st
 # Leer los datos
 car_data = pd.read_csv(r'C:\Users\Lorenzo\Desktop\ORDER_L8R\Data_Analysis\TripleTen\Sprints\Sprint_4\Notebooks\Capitulo_7\proyecto_sprint_4\vehicles_us.csv', sep=',')
 
-# Crear un botón para construir un histograma
-hist_button = st.button('Construir histograma')
+# Crear una casilla de verificación para el histograma
+build_histogram = st.checkbox('Construir un histograma')
 
-if hist_button: # al hacer clic en el botón del histograma
+if build_histogram: # Si la casilla de verificación del histograma está seleccionada
     # Escribir un mensaje
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     
     # Crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, x="odometer", title="Histograma de Odómetro")
     
     # Mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
-# Crear otro botón para construir un gráfico de dispersión
-scatter_button = st.button('Construir gráfico de dispersión')
+# Crear una casilla de verificación para el gráfico de dispersión
+build_scatter_plot = st.checkbox('Construir gráfico de dispersión')
 
-if scatter_button: # al hacer clic en el botón del gráfico de dispersión
+if build_scatter_plot: # Si la casilla de verificación del gráfico de dispersión está seleccionada
     # Escribir un mensaje
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
     
